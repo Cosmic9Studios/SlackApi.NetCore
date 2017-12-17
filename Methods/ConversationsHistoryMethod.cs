d21e2b8a4b1d7a952fcf96656ff9eb6d0cd82449
Peter Henry <peterhenry20@live.com>
2017-12-17 03:44:56 -0500

    Changed IMethod to a class; Parameters now set in property setters

--- Methods/ConversationsHistoryMethod.cs
+++ Methods/ConversationsHistoryMethod.cs
@@ -2,14 +2,11 @@ using System.Collections.Generic;
 
 namespace SlackApi.Methods
 {
-    public class ConversationsHistoryMethod : IMethod
+    public class ConversationsHistoryMethod : Method
     {
         public ConversationsHistoryMethod(string channel)
         {
-            Parameters = new List<KeyValuePair<string, string>>();
             Parameters.Add(new KeyValuePair<string, string>("channel", channel));
         }
-
-        public List<KeyValuePair<string, string>> Parameters { get; }
     }
 }
--- Methods/UsersIdentityMethod.cs
+++ Methods/UsersIdentityMethod.cs
@@ -1,10 +1,4 @@
-using System.Collections.Generic;
-using Newtonsoft.Json;
-
 namespace SlackApi.Methods
 {
-    public class UsersIdentityMethod : IMethod
-    {
-        public List<KeyValuePair<string, string>> Parameters => new List<KeyValuePair<string, string>>();
-    }
+    public class UsersIdentityMethod : Method { }
 }
