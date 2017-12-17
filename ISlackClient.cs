e1d58be77cf5539e577c45fe43bf3236a501e91b
Peter Henry <peterhenry20@live.com>
2017-12-17 03:48:17 -0500

    Added Disconnect method; Token can now be set anywhere.

--- ISlackClient.cs
+++ ISlackClient.cs
@@ -10,16 +10,21 @@ namespace SlackApi
     public interface ISlackClient
     {
         /// <summary>
-        /// Connects to the slack client.
+        /// Connects to the slack api.
         /// </summary>
         /// <param name="token">The token used to identify the user.</param>
         Task<ConnectResponse> Connect(string token);
 
         /// <summary>
+        /// Disconnects from the slack api.
+        /// </summary>
+        void Disconnect();
+
+        /// <summary>
         /// Calls a web api method.
         /// </summary>
         /// <param name="method">The method to invoke.</param>
-        Task<T> CallApiMethod<T>(IMethod method) where T: Response;
+        Task<T> CallApiMethod<T>(Method method) where T: Response;
         
         /// <summary>
         /// Binds a callback to an event.
