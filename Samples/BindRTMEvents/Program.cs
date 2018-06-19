7d4db6d05d06aee2cb7e10dc08bb5135ce8c7ad0
Grayson Powers <arthurgpowers@gmail.com>
2018-06-18 20:46:21 -0500

    Added samples for usage of SlackRTM Events and SlackAPI Methods

---
+++ Samples/BindRTMEvents/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using SlackApi;
+using SlackApi.Events;
+
+namespace BindRTMEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var slackClient = new SlackClient("Slack User Token");
+            var connectionResponse = Task.Run(() => slackClient.Connect()).Result;
+
+            if (connectionResponse.Ok)
+            {
+                slackClient.BindEvent<ReactionAddedEvent>(ReactionAddedCallback);
+                slackClient.BindEvent<ReactionRemovedEvent>(ReactionRemovedCallback);
+
+                Console.WriteLine("User connected");
+            }
+
+            Console.Read();
+        }
+
+        private static void ReactionAddedCallback(ReactionAddedEvent reactionAddedData)
+        {
+            Console.WriteLine($"{ reactionAddedData.Reaction } added by { reactionAddedData.User }");
+        }
+
+        private static void ReactionRemovedCallback(ReactionRemovedEvent reactionRemovedData)
+        {
+            Console.WriteLine($"{ reactionRemovedData.Reaction } removed by { reactionRemovedData.User }");
+        }
+    }
+}
---
+++ Samples/CallAPIMethods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using SlackApi;
+using SlackApi.Methods;
+using SlackApi.Responses;
+
+namespace CallAPIMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tempAuthCode = "Temporary authorization code given by Slack when a user authorizes your app";
+            var oauthAccessMethod = new OauthAccessMethod("Slack app client ID", "Slack app client secret", tempAuthCode)
+            {
+                RedirectUri = "Slack app authorized redirect URL"
+            };
+            var slackClient = Task.Run(() => AuthorizeClient(oauthAccessMethod)).Result;
+
+            Task.Run(() => TestAuth(slackClient));
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Exchange the temporary code for the user's secret token.
+        /// </summary>
+        /// <param name="oauthAccessMethod">OAuth access method options.</param>
+        /// <returns>Client that will allow taking authorized actions on behalf of the user.</returns>
+        private static async Task<SlackClient> AuthorizeClient(OauthAccessMethod oauthAccessMethod)
+        {
+            var tempClient = new SlackClient("");
+            var oauthResponse = await tempClient.CallApiMethod<OauthAccessResponse>(oauthAccessMethod);
+            
+            return new SlackClient(oauthResponse.AccessToken);
+        }
+
+        /// <summary>
+        /// Check the client's basic authentication and identity information.
+        /// </summary>
+        private static async Task TestAuth(ISlackClient slackClient)
+        {
+            var authTestResponse = await slackClient.CallApiMethod<AuthTestReponse>(new AuthTestMethod());
+
+            if (authTestResponse.Ok)
+            {
+                Console.WriteLine($"{ authTestResponse.User } has been authorized.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to authorize user - { authTestResponse.Error }");
+            }
+        }
+    }
+}
